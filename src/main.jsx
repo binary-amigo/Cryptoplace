8f3eebfc478462c1bf6e4bc0608e017810c64e12
Imran Khan <imrankh8602@gmail.com>
2024-06-09 09:58:50 +0530

    Using HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import CoinContextProvider from "./context/CoinContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
+  <HashRouter>
     <CoinContextProvider>
       <App />
     </CoinContextProvider>
-  </BrowserRouter>
+  </HashRouter>
+
 );
